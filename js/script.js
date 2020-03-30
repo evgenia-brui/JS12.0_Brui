bacc5f1cc9c4669959bc6cb0d45616836b78e52a
Evgenia Brui <j-lp@mail.ru>
2020-03-30 21:44:25 +0300

    Новый js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-const money = 500, // любое число “Доход за месяц”
+const money = +prompt('Ваш месячный доход?', 500), // любое число “Доход за месяц”
     income = '5', // строка с дополнительными доходом (например: фриланс)
-    addExpenses = '35, 10, 70', // строка (интернет, такси, коммуналка)
-    deposit = true; // любое булево значение
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'бензин, подарки'), // строка (интернет, такси, коммуналка)
+    deposit = confirm('Есть ли у вас депозит в банке?'), // любое булево значение
+    expenses1 = prompt('Введите обязательную статью расходов', 'интернет'),
+    amount1 = +prompt('Во сколько это обойдется?', 35),
+    expenses2 = prompt('Введите обязательную статью расходов', 'коммуналка'),
+    amount2 = +prompt('Во сколько это обойдется?', 70),
     mission = 4000, // любое число (Какую сумму хотите накопить)
     period = 12; // число от 1 до 12 (месяцев)
 
@@ -14,11 +18,29 @@ console.info('Длина строки addExpenses');
 console.log(addExpenses.length);
 
 console.info('Период равен ' + period + ' месяцев');
-console.info('Цель накопить ' + mission + ' юани');
+console.info('Цель накопить ' + mission + ' BYN');
 
 console.info('Привести строку addExpenses к нижнему регистру и разбить строку на массив');
 console.log(addExpenses.toLowerCase().split(', '));
 
-console.info('Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)');
-const budgetDay = money / 30;
-console.log(budgetDay);
+const budgetMonth = money - (amount1 + amount2);
+console.log('Бюджет на месяц: ' + budgetMonth + ' BYN');
+
+const budgetDay = budgetMonth / 30;
+console.log('Бюджет на день: ' + Math.floor(budgetDay) + ' BYN');
+
+const missionComplete = Math.ceil(mission / budgetMonth);
+console.log('Цель будет достигнута за ' + missionComplete + ' мес');
+
+let yourIncomeLevel = '';
+if (budgetDay >= 1200) {
+    yourIncomeLevel = 'У вас высокий уровень дохода';
+} else if (budgetDay >= 600) {
+    yourIncomeLevel = 'У вас средний уровень дохода';
+} else if (budgetDay >= 0) {
+    yourIncomeLevel = 'К сожалению у вас уровень дохода ниже среднего';
+} else {
+    yourIncomeLevel = 'Что-то пошло не так';
+}
+
+console.log(yourIncomeLevel);
